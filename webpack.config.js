6848b1f8303ff694cdee753094a9e1f9542e1b53
scxavier <shiju.c.xavier@eqs.com>
2020-08-30 17:22:26 +0530

    payment page done

--- webpack.config.js
+++ webpack.config.js
@@ -118,7 +118,7 @@ module.exports = {
       // This cleans the dist folder before each build, so that only used files will be available
       new CleanWebpackPlugin(['dist']),
       new BrowserSyncPlugin({
-            proxy: 'scx-ind.irpages2.equitystory/shiju/pages/dashboard-provider/provider-requests.html'
+            proxy: 'scx-ind.irpages2.equitystory/shiju/pages/dashboard-provider/payment.html'
       })
   ]
 };
